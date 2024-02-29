4192e3ab009a8827787fb47e2d1e7c356e28cf5f
Meet Vasani <meet.vasani86@gmail.com>
2024-03-01 02:47:11 +0530

    feat: add logic to handle probes in the container

--- src/controllers/dpcontroller.rs
+++ src/controllers/dpcontroller.rs
@@ -3,9 +3,14 @@ use std::{collections::BTreeMap, sync::Arc};
 use k8s_openapi::{
     api::{
         apps::v1::{Deployment, DeploymentSpec},
-        core::v1::{Container, PodSpec, PodTemplateSpec, ResourceRequirements},
+        core::v1::{
+            Container, ExecAction, HTTPGetAction, PodSpec, PodTemplateSpec, Probe,
+            ResourceRequirements, TCPSocketAction,
+        },
     },
-    apimachinery::pkg::{api::resource::Quantity, apis::meta::v1::LabelSelector},
+    apimachinery::pkg::{
+        api::resource::Quantity, apis::meta::v1::LabelSelector, util::intstr::IntOrString,
+    },
 };
 use kube::{
     api::{ObjectMeta, Patch, PatchParams},
@@ -13,7 +18,9 @@ use kube::{
     Api, Resource,
 };
 
-use crate::crds::nimble::{ContainerSpec, Nimble};
+use crate::crds::{deploymentspec::ProbeSpec, nimble::Nimble};
+
+use crate::crds::deploymentspec::ContainerSpec;
 
 use crate::common::client::{error_policy, ContextData, Error};
 
@@ -23,6 +30,49 @@ use futures::StreamExt;
 
 use tracing::{error, info};
 
+pub fn transform_probe(probe_type: &Option<ProbeSpec>) -> Option<Probe> {
+    if let Some(probe) = probe_type {
+        let mut shared_probe = Probe {
+            period_seconds: probe.period_seconds,
+            success_threshold: probe.success_threshold,
+            initial_delay_seconds: probe.initial_delay_seconds,
+            ..Probe::default()
+        };
+
+        match (
+            probe.exec.clone(),
+            probe.http_get.clone(),
+            probe.tcp_socket.clone(),
+        ) {
+            (Some(cmd), None, None) => {
+                shared_probe.exec = Some(ExecAction { command: Some(cmd) });
+                
+                Some(shared_probe)
+            }
+            (None, Some(http_get), None) => {
+                shared_probe.http_get = Some(HTTPGetAction {
+                    path: Some(http_get.path),
+                    port: IntOrString::Int(http_get.port),
+                    ..HTTPGetAction::default()
+                });
+
+                Some(shared_probe)
+            }
+            (None, None, Some(tcp_sock)) => {
+                shared_probe.tcp_socket = Some(TCPSocketAction {
+                    port: IntOrString::Int(tcp_sock.port),
+                    ..TCPSocketAction::default()
+                });
+
+                Some(shared_probe)
+            }
+            _ =>  None,
+        }
+    } else {
+        None
+    }
+}
+
 /// Transforms struct `ContainerSpec` to `Container` Vec that is required in `PodSpec`
 /// Returns Vec of `Container`.
 /// # Arguments
@@ -30,31 +80,32 @@ use tracing::{error, info};
 pub fn transform_containers(container_spec: Vec<ContainerSpec>) -> Vec<Container> {
     let containers: Vec<Container> = container_spec
         .iter()
-        .map(|spec| Container {
-            name: spec.name.clone(),
-            image: Some(spec.image.clone()),
-            command: spec.command.clone(),
-            resources: Some(ResourceRequirements {
-                requests: Some(BTreeMap::from([
-                    (
-                        "memory".to_owned(),
-                        Quantity(spec.memory_request.clone().unwrap()),
-                    ),
-                    (
-                        "cpu".to_owned(),
-                        Quantity(spec.cpu_request.clone().unwrap()),
-                    ),
-                ])),
-                limits: Some(BTreeMap::from([
-                    (
-                        "memory".to_owned(),
-                        Quantity(spec.memory_limit.clone().unwrap()),
-                    ),
-                    ("cpu".to_owned(), Quantity(spec.cpu_limit.clone().unwrap())),
-                ])),
-                ..ResourceRequirements::default()
-            }),
-            ..Container::default()
+        .map(|spec| -> Container {
+            let mut container = Container {
+                name: spec.name.clone(),
+                image: Some(spec.image.clone()),
+                command: spec.command.clone(),
+                resources: Some(ResourceRequirements {
+                    requests: Some(BTreeMap::from([
+                        ("memory".to_owned(), Quantity(spec.memory_request.clone())),
+                        ("cpu".to_owned(), Quantity(spec.cpu_request.clone())),
+                    ])),
+                    limits: Some(BTreeMap::from([
+                        ("memory".to_owned(), Quantity(spec.memory_limit.clone())),
+                        ("cpu".to_owned(), Quantity(spec.cpu_limit.clone())),
+                    ])),
+                    ..ResourceRequirements::default()
+                }),
+                ..Container::default()
+            };
+
+            // configure available probes.
+            container.liveness_probe = transform_probe(&spec.liveness_probe);
+            container.readiness_probe = transform_probe(&spec.readiness_probe);
+            container.startup_probe = transform_probe(&spec.startup_probe);
+
+            // return container.
+            container
         })
         .collect();
 
