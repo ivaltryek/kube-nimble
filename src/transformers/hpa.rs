ed77ef3c7b40b26a9d47e11f0c5b354c54cbacfa
Meet Vasani <meet.vasani86@gmail.com>
2024-03-15 03:15:50 +0530

    chore: move creation of object to transformers

--- src/transformers/hpa.rs
+++ src/transformers/hpa.rs
@@ -1,8 +1,14 @@
-use k8s_openapi::api::autoscaling::v2::{MetricSpec, MetricTarget, ResourceMetricSource};
+use std::sync::Arc;
 
-use crate::crds::hpaspec::HPASpec;
+use k8s_openapi::api::autoscaling::v2::{
+    CrossVersionObjectReference, HorizontalPodAutoscaler, HorizontalPodAutoscalerSpec, MetricSpec,
+    MetricTarget, ResourceMetricSource,
+};
+use kube::{api::ObjectMeta, Resource};
 
-pub fn transform_metrics(hpa_spec: Option<HPASpec>) -> Option<Vec<MetricSpec>> {
+use crate::crds::{hpaspec::HPASpec, nimble::Nimble};
+
+fn transform_metrics(hpa_spec: Option<HPASpec>) -> Option<Vec<MetricSpec>> {
     let mut metric_spec_vec = Vec::new();
 
     match hpa_spec.clone() {
@@ -25,4 +31,38 @@ pub fn transform_metrics(hpa_spec: Option<HPASpec>) -> Option<Vec<MetricSpec>> {
         }
         _ => None,
     }
+}
+
+pub fn transform_hpa(nimble: Arc<Nimble>, is_dry_run: bool) -> HorizontalPodAutoscaler {
+    let hpa_spec = nimble.spec.hpa.clone().unwrap();
+    let hpa: HorizontalPodAutoscaler = HorizontalPodAutoscaler {
+        metadata: if is_dry_run {
+            ObjectMeta {
+                name: nimble.metadata.name.clone(),
+                annotations: hpa_spec.annotations.clone(),
+                ..ObjectMeta::default()
+            }
+        } else {
+            let oref = nimble.controller_owner_ref(&()).unwrap();
+            ObjectMeta {
+                name: nimble.metadata.name.clone(),
+                owner_references: Some(vec![oref]),
+                annotations: hpa_spec.annotations.clone(),
+                ..ObjectMeta::default()
+            }
+        },
+        spec: Some(HorizontalPodAutoscalerSpec {
+            max_replicas: hpa_spec.max,
+            min_replicas: hpa_spec.min,
+            scale_target_ref: CrossVersionObjectReference {
+                api_version: Some("apps/v1".to_owned()),
+                kind: "Deployment".to_owned(),
+                name: nimble.metadata.name.clone().unwrap(),
+            },
+            metrics: transform_metrics(Some(hpa_spec)),
+            ..HorizontalPodAutoscalerSpec::default()
+        }),
+        ..HorizontalPodAutoscaler::default()
+    };
+    hpa
 }
